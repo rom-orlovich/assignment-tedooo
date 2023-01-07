7624193a6f075ce22b9e4ae4927ef7d01631e917
Rom Orlovich <romorlovich@gmail.com>
2023-01-07 21:40:18 +0200

    add navbar links into the navbar

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { createTheme, ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./tsx/App";
 import { store } from "./redux/store";
+import { APP_ROUTES } from "./tsx/components/NavBar/NavBar";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -16,7 +18,18 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <App />
+        <RouterProvider
+          router={createBrowserRouter([
+            {
+              path: "/",
+              element: <App />,
+              children: [
+                { path: APP_ROUTES.MESSAGES_ROUTE, element: <App /> },
+                { path: APP_ROUTES.NOTIFICATION_ROUTE, element: <App /> },
+              ],
+            },
+          ])}
+        />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
--- src/client/src/tsx/components/NavBar/NavBar.tsx
+++ src/client/src/tsx/components/NavBar/NavBar.tsx
@@ -1,24 +1,31 @@
 import {
   AppBar,
   Toolbar,
-  Typography,
   IconButton,
   Autocomplete,
-  TextField,
   Stack,
   SxProps,
-  InputAdornment,
   List,
   ListItem,
+  ListItemIcon,
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import { grey } from "@mui/material/colors";
-import { ReactElement } from "react";
+
+import { NavLink } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import { ReactNode, useState } from "react";
 import TedoooIcon from "./TedoooIcon";
 
+export enum APP_ROUTES {
+  MESSAGES_ROUTE = "messages",
+  NOTIFICATION_ROUTE = "notifications",
+}
 function NavBar() {
   const AppBarSX: SxProps = {
     "&": {
+      width: "100%",
       backgroundColor: "white",
       ".navbar-left": {
         ".tedooo-logo": {
@@ -29,12 +36,7 @@ function NavBar() {
             "linear-gradient(131.96deg, #96DCD0 -8.63%, #6CCEBD -5.56%, #2DB8A1 35.83%, #28A38F 89.48%, #208171 133.94%)",
           borderRadius: "4px",
         },
-        // ".MuiOutlinedInput-root": {
-        //   padding: "1px",
-        //   borderRadius: "32px",
-        //   background: "#F4F5F5",
-        //   border: "none",
-        // },
+
         ".search-input": {
           position: "relative",
           svg: {
@@ -58,57 +60,88 @@ function NavBar() {
           },
         },
       },
-      ".navbar-right": {},
+      ".navbar-right": {
+        li: {
+          padding: "0.5rem",
+        },
+        ml: "auto",
+        a: {
+          padding: "0 0.5rem",
+          textDecoration: "none",
+        },
+        "li:has(.active)": {
+          background: "",
+          "*": {
+            color: "#2DB8A1",
+          },
+          borderBottom: "2px solid #2DB8A1",
+        },
+      },
     },
   };
   interface Link {
     to: string;
-    icon?: () => JSX.Element;
+    icon?: ReactNode;
     text: string;
   }
+
   const navbarLinks: Link[] = [
-    { to: "/home", text: "Home" },
-    { to: "/messages", text: "Messages" },
-    { to: "/notifications", text: "Notifications" },
+    { to: "/", text: "Home", icon: <HomeOutlinedIcon /> },
+    {
+      to: APP_ROUTES.MESSAGES_ROUTE,
+      text: "Messages",
+      icon: <ChatBubbleOutlineOutlinedIcon />,
+    },
+    {
+      to: APP_ROUTES.NOTIFICATION_ROUTE,
+      text: "Notifications",
+      icon: <NotificationsNoneOutlinedIcon />,
+    },
   ];
   return (
     <AppBar sx={AppBarSX} position="fixed">
-      <Toolbar variant="regular">
-        <Stack>
-          <Stack direction={"row"} alignItems="center" className="navbar-left">
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <TedoooIcon />
-            </IconButton>
-            <Autocomplete
-              id="free-solo-demo"
-              freeSolo
-              options={["s"]}
-              className="search-input"
-              renderInput={(params) => (
-                <div ref={params.InputProps.ref}>
-                  <SearchIcon />
-                  <input
-                    type="text"
-                    {...params.inputProps}
-                    placeholder={"Search"}
-                  />
-                </div>
-              )}
-            />
-          </Stack>
+      <Toolbar component={Stack} variant="regular" direction={"row"}>
+        <Stack direction={"row"} alignItems="center" className="navbar-left">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <TedoooIcon />
+          </IconButton>
+          <Autocomplete
+            id="free-solo-demo"
+            freeSolo
+            options={["s"]}
+            className="search-input"
+            renderInput={(params) => (
+              <div ref={params.InputProps.ref}>
+                <SearchIcon />
+                <input
+                  type="text"
+                  {...params.inputProps}
+                  placeholder={"Search"}
+                />
+              </div>
+            )}
+          />
+        </Stack>
 
-          <Stack className="navbar-right" direction="row">
-            <List>
-              {navbarLinks.map((link) => (
-                <ListItem> </ListItem>
-              ))}
-            </List>
-          </Stack>
+        <Stack component={"ul"} className="navbar-right" direction="row">
+          {navbarLinks.map(({ to, text, icon }) => (
+            <ListItem key={to}>
+              {icon && (
+                <ListItemIcon sx={{ minWidth: "2rem" }}>{icon}</ListItemIcon>
+              )}
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : "unactive")}
+                to={to}
+              >
+                {text}
+              </NavLink>
+            </ListItem>
+          ))}
         </Stack>
       </Toolbar>
     </AppBar>
