d36a8783cdec5778e551dd10c37ecd3dbd78da0c
Rom Orlovich <romorlovich@gmail.com>
2023-01-07 18:03:49 +0200

    add infinte scrolling to the feed

--- src/client/src/tsx/components/Feed/Feed.tsx
+++ src/client/src/tsx/components/Feed/Feed.tsx
@@ -1,11 +1,12 @@
 import { Box, SxProps } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import { postsAPI } from "../../../redux/api/postsAPI";
 import Post from "./Post/Post";
 
 function Feed() {
   const { isLoading, isFetching, data } = postsAPI.useGetPostsQuery({});
-
+  const [page, setPage] = useState(0);
   if (isLoading || isFetching) return <>loading</>;
   const feedSX: SxProps = {
     "&": {
@@ -16,12 +17,21 @@ function Feed() {
       gap: "0.5rem",
     },
   };
+
   return (
-    <Box sx={feedSX}>
-      {data?.data.slice(0, 6)?.map((post) => (
-        <Post key={post.id} {...post} />
-      ))}
-    </Box>
+    <InfiniteScroll
+      pageStart={0}
+      hasMore={page * 6 < (data?.data?.length || 0)}
+      loadMore={(page) => {
+        setPage((prePage) => prePage + 1);
+      }}
+    >
+      <Box sx={feedSX}>
+        {data?.data.slice(0 * page, 6 * page + 1)?.map((post) => (
+          <Post key={post.id} {...post} />
+        ))}
+      </Box>
+    </InfiniteScroll>
   );
 }
 
