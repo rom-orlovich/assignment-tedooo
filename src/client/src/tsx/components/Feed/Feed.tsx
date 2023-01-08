e925146d722ff9bd76ac6db5d8de7549c4011220
Rom Orlovich <romorlovich@gmail.com>
2023-01-08 12:22:13 +0200

    refactor Feed.tsx in client

--- src/client/src/tsx/components/Feed/Feed.tsx
+++ src/client/src/tsx/components/Feed/Feed.tsx
@@ -1,49 +1,48 @@
 import { Box, SxProps } from "@mui/material";
 import React, { useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
-import { postsAPI } from "../../../redux/api/postsAPI";
+import { PostAPI, postsAPI } from "../../../redux/api/postsAPI";
 import { useAppSelector } from "../../../redux/hooks";
 import { getFeedFilters } from "../../../redux/slices/feedFiltersSlice";
 import Post from "./Post/Post";
 
+export const postFilterMatch = (post: PostAPI, searchQuery: string) =>
+  (post.shopName || post.username)
+    .toLowerCase()
+    ?.startsWith(searchQuery.toLowerCase());
+
+const feedSX: SxProps = {
+  "&": {
+    width: "75%",
+    margin: "0 auto",
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.5rem",
+    pt: "5rem",
+  },
+};
 function Feed() {
   const { isLoading, isFetching, data } = postsAPI.useGetPostsQuery({});
   const [page, setPage] = useState(0);
-  const { searchValue } = useAppSelector(getFeedFilters);
+  const { searchQuery } = useAppSelector(getFeedFilters);
   if (isLoading || isFetching) return <>loading</>;
-  const feedSX: SxProps = {
-    "&": {
-      width: "75%",
-      margin: "0 auto",
-      display: "flex",
-      flexDirection: "column",
-      gap: "0.5rem",
-      pt: "5rem",
-    },
+
+  const handlePostsLoader = () => {
+    setPage((page) => page + 1);
   };
+
+  const postsList = data?.data
+    .filter((post) => (searchQuery ? postFilterMatch(post, searchQuery) : true))
+    .slice(0, 6 * (page + 1))
+    ?.map((post, index) => <Post key={post.id} {...post} index={index} />);
 
   return (
     <InfiniteScroll
       pageStart={page}
       hasMore={page * 6 < (data?.data?.length || 0)}
-      loadMore={() => {
-        setPage((page) => page + 1);
-      }}
+      loadMore={handlePostsLoader}
     >
-      <Box sx={feedSX}>
-        {data?.data
-          .filter((post) =>
-            searchValue
-              ? (post.shopName || post.username)
-                  .toLowerCase()
-                  ?.startsWith(searchValue.toLowerCase())
-              : true
-          )
-          .slice(0, 6 * (page + 1))
-          ?.map((post, index) => (
-            <Post key={post.id} {...post} index={index} />
-          ))}
-      </Box>
+      <Box sx={feedSX}>{postsList}</Box>
     </InfiniteScroll>
   );
 }
